b63ef394768cbd3730e9779c8642d8806445c9d7
eceylaan <ceylann.ece@gmail.com>
2024-10-30 22:06:21 +0300

    cards düzeltme

--- components/cards/Cards.jsx
+++ components/cards/Cards.jsx
@@ -8,25 +8,23 @@ export default async function Cards() {
   const data = response || []; // Eğer response null veya undefined ise boş bir dizi ile başla
 
   return (
-    <div className="cardsContainer">
-      <div className="likesnContentBox">
-        {data.map((x) => (
-          <div key={x.id} className="contentBox">
-            {" "}
-            {/* Anahtar burada div için kullanılmalı */}
+    <div className="containerC">
+      {data.map((x) => (
+        <div key={x.id} className="cardsContainer">
+          <div className="leftcontentCard">
             <Likes voteCount={x.voteCount} />
             <div className="content">
               <p className="title">{x.title}</p>
               <p className="text">{x.description}</p>
               <p className="categories">{x.status}</p>
             </div>
-            <div className="commentBox">
-              <Image width={18} height={18} src="/assets/comment-icon.svg" alt="commentIcon" />
-              <p className="commentCount">{x.comments}</p>
-            </div>
           </div>
-        ))}
-      </div>
+          <div className="commentBox">
+            <Image width={18} height={18} src="/assets/comment-icon.svg" alt="commentIcon" />
+            <p className="commentCount">{x.comments}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
