2d21bd05cd067b332145ba6873af9c861a62a0d3
eceylaan <ceylann.ece@gmail.com>
2024-10-30 20:43:19 +0300

    dcs

--- components/cards/Cards.jsx
+++ components/cards/Cards.jsx
@@ -6,12 +6,14 @@ import { getFeedback } from "@/utils/fetch";
 export default async function Cards() {
   const { response } = await getFeedback();
   const data = response || []; // Eğer response null veya undefined ise boş bir dizi ile başla
-  
+
   return (
     <div className="cardsContainer">
       <div className="likesnContentBox">
         {data.map((x) => (
-          <div key={x.id} className="contentBox"> {/* Anahtar burada div için kullanılmalı */}
+          <div key={x.id} className="contentBox">
+            {" "}
+            {/* Anahtar burada div için kullanılmalı */}
             <Likes voteCount={x.voteCount} />
             <div className="content">
               <p className="title">{x.title}</p>
@@ -19,12 +21,7 @@ export default async function Cards() {
               <p className="categories">{x.status}</p>
             </div>
             <div className="commentBox">
-              <Image
-                width={18}
-                height={18}
-                src="/assets/comment-icon.svg"
-                alt="commentIcon"
-              />
+              <Image width={18} height={18} src="/assets/comment-icon.svg" alt="commentIcon" />
               <p className="commentCount">{x.comments}</p>
             </div>
           </div>
