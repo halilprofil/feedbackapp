1f5fcb4d29f4b7152a3ca7781639b6ef27ab617d
“[halil <halilprofil@gmail.com>
2025-01-20 23:19:53 +0300

    update for vercel 10

--- app/roadmap/page.jsx
+++ app/roadmap/page.jsx
@@ -4,8 +4,8 @@ import { AdvancedFetch } from "@/utils/advancedfetch";
 export default async function RoadMap() {
   const { response } = await AdvancedFetch("https://feedback.nazlisunay.com.tr/api/Opinions");
   const data = await response ;
-  const statusPlanned = data.filter((x) => x.status === "Planned");
-  const statusProgress = data.filter((x) => x.status === "InProgress");
-  const statusLive = data.filter((x) => x.status === "Live");
+  const statusPlanned = data?.filter((x) => x.status === "Planned");
+  const statusProgress = data?.filter((x) => x.status === "InProgress");
+  const statusLive = data?.filter((x) => x.status === "Live");
   return <RoadMapsCards statusPlanned={statusPlanned} statusProgress={statusProgress} statusLive={statusLive} />;
 }
