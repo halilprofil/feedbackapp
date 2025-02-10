1a49a9009432bd21fc855e1305b3e678f177052c
“[halil <halilprofil@gmail.com>
2025-02-10 19:36:50 +0300

    logo for vercel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Toaster } from "sonner";
 import "./globals.css";
 import { Jost } from "next/font/google";
+import Head from "next/head";  // Head bileşenini içe aktarın
 
 const jost = Jost({ subsets: ["latin"] });
 
@@ -13,9 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${jost.className}`}>
+        {/* Favicon ekle */}
+        <Head>
+          <link rel="icon" href="/assets/logo.svg"/>
+        </Head>
+
         {children}
         <Toaster position="top-center" />
       </body>
     </html>
   );
 }
+
